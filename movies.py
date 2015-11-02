148d534c91c4972aa2621acc34fca26d1f2c52e6
Manish Makwana <manish.makwana@live.com>
2015-11-02 22:30:09 +1300

    added MIT licence, blank readme file, and made a bunch of formating and documentation changes (nothing functional except moving the executing code to a main function, for pydoc compatibility).

--- movies.py
+++ movies.py
@@ -1,26 +1,50 @@
-#collect info on favourite movies then display them as an html page with links to trailers
+#/usr/bin/python
+"""
+Collect info on favourite movies then display them as an html page with
+links to trailers.
+"""
+
 
 import webbrowser
 import fresh_tomatoes
 
-class Movie:
-	def __init__(self, movie_title, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-	
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
 
-interstellar = Movie(movie_title="Interstellar",
-	poster_image="http://fangirlnation.com/wp-content/uploads/2014/10/interstellar-poster.jpg",
-	trailer_youtube="https://www.youtube.com/watch?v=zSWdZVtXT7E")
+class Movie(object):
+    """
+    Return an object with movie metadata.
+    
+    Attributes:
+        movie_title: Title of the movie.
+        poster_image: URL to an image of the movie's poster.
+        trailer_youtube: URL to a youtube video of the official trailer.
+    """
 
-print(interstellar.__dict__)
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-eot = Movie(movie_title = "Edge of Tomorrow",
-	poster_image = "http://www.graffitiwithpunctuation.net/wp-content/uploads/2014/06/Edge_of_Tomorrow_live-die-repeat.jpg",
-	trailer_youtube = "https://www.youtube.com/watch?v=yUmSVcttXnI")
-print(eot.__dict__)
-movies = [interstellar, eot]
-fresh_tomatoes.open_movies_page(movies)
+    def show_trailer(self):
+        """Open the film trailer in the system default web browser."""
+        webbrowser.open(self.trailer_youtube_url)
+
+
+def main():
+    # Create a list of favourite movies, with relevant metadata loaded.
+    INTERSTELLAR = Movie(
+        movie_title = "Interstellar",
+        poster_image = ("http://fangirlnation.com/wp-content/"
+                        "uploads/2014/10/interstellar-poster.jpg"),
+        trailer_youtube = "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+    EOT = Movie(
+        movie_title = "Edge of Tomorrow",
+        poster_image = ("http://www.graffitiwithpunctuation.net/wp-content/"
+                        "uploads/2014/06/Edge_of_Tomorrow_live-die-repeat.jpg"),
+        trailer_youtube = "https://www.youtube.com/watch?v=yUmSVcttXnI")
+    MOVIES = [INTERSTELLAR, EOT]
+
+    # Generate an HTML file and load in the movie information.
+    fresh_tomatoes.open_movies_page(MOVIES)
+
+if __name__ == '__main__':
+        main()
