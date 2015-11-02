027760bd244425ab61b783e0ccdea48bf098df49
Manish Makwana <manish.makwana@live.com>
2015-11-02 22:46:33 +1300

    further formatting to get 10/10 on pylint.

--- movies.py
+++ movies.py
@@ -9,10 +9,10 @@ import webbrowser
 import fresh_tomatoes
 
 
-class Movie(object):
+class Movie(object):    # pylint: disable=too-few-public-methods
     """
     Return an object with movie metadata.
-    
+
     Attributes:
         movie_title: Title of the movie.
         poster_image: URL to an image of the movie's poster.
@@ -30,21 +30,22 @@ class Movie(object):
 
 
 def main():
+    """Main function."""
     # Create a list of favourite movies, with relevant metadata loaded.
-    INTERSTELLAR = Movie(
-        movie_title = "Interstellar",
-        poster_image = ("http://fangirlnation.com/wp-content/"
-                        "uploads/2014/10/interstellar-poster.jpg"),
-        trailer_youtube = "https://www.youtube.com/watch?v=zSWdZVtXT7E")
-    EOT = Movie(
-        movie_title = "Edge of Tomorrow",
-        poster_image = ("http://www.graffitiwithpunctuation.net/wp-content/"
-                        "uploads/2014/06/Edge_of_Tomorrow_live-die-repeat.jpg"),
-        trailer_youtube = "https://www.youtube.com/watch?v=yUmSVcttXnI")
-    MOVIES = [INTERSTELLAR, EOT]
+    interstellar = Movie(
+        movie_title="Interstellar",
+        poster_image=("http://fangirlnation.com/wp-content/"
+                      "uploads/2014/10/interstellar-poster.jpg"),
+        trailer_youtube="https://www.youtube.com/watch?v=zSWdZVtXT7E")
+    edge_of_tomorrow = Movie(
+        movie_title="Edge of Tomorrow",
+        poster_image=("http://www.graffitiwithpunctuation.net/wp-content/"
+                      "uploads/2014/06/Edge_of_Tomorrow_live-die-repeat.jpg"),
+        trailer_youtube="https://www.youtube.com/watch?v=yUmSVcttXnI")
+    movies = [interstellar, edge_of_tomorrow]
 
     # Generate an HTML file and load in the movie information.
-    fresh_tomatoes.open_movies_page(MOVIES)
+    fresh_tomatoes.open_movies_page(movies)
 
 if __name__ == '__main__':
-        main()
+    main()
